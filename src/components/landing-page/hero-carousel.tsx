ea81b536ed76df88ea528fb84d859579a905bffe
Maelton Lima <maelton.main@gmail.com>
2025-04-09 12:50:09 -0300

    refactor: adjust hero carousel title layout for improved presentation
    - modify the title structure by adding a line break between 'Associação de Artes Marciais' and 'Budokan-Ryu' to enhance visual separation and emphasis.

--- src/components/landing-page/hero-carousel.tsx
+++ src/components/landing-page/hero-carousel.tsx
@@ -118,8 +118,7 @@ export default function HeroCarousel() {
               </div>
               <h1 className='mb-4 text-4xl font-bold md:text-6xl'>
                 Associação de Artes Marciais
-              </h1>
-              <h1 className='mb-4 text-4xl font-bold md:text-6xl'>
+                <br/>
                 Budokan-Ryu
               </h1>
               <h2 className='mb-2 text-2xl font-semibold md:text-3xl'>
