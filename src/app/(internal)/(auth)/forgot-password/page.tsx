61c5d8f03e504e18e3fbfdb072162f991b0abc9c
Filipe Zaidan <felipezaidan10@gmail.com>
2025-07-01 13:27:20 -0300

    refactor: remove Suspense wrapper from authentication pages for simplified rendering
    - Eliminated the Suspense component from both ForgotPasswordPage and LoginPage to streamline rendering.
    - Updated the structure of the pages to directly return their respective content components.

--- src/app/(internal)/(auth)/forgot-password/page.tsx
+++ src/app/(internal)/(auth)/forgot-password/page.tsx
@@ -5,9 +5,7 @@ import {
   ResetPasswordForm,
 } from '@/components/auth/forgot-password/forgot-password-form';
 import { useSearchParams } from 'next/navigation';
-import { Suspense } from 'react';
 
-// Component that uses useSearchParams
 function ForgotPasswordContent() {
   const searchParams = useSearchParams();
   const token = searchParams.get('token');
@@ -19,20 +17,6 @@ function ForgotPasswordContent() {
   return <ForgotPasswordForm />;
 }
 
-// Loading component
-function Loading() {
-  return (
-    <div className='flex min-h-screen items-center justify-center'>
-      <div className='h-8 w-8 animate-spin rounded-full border-b-2 border-gray-900'></div>
-    </div>
-  );
-}
-
-// Main page component
 export default function ForgotPasswordPage() {
-  return (
-    <Suspense fallback={<Loading />}>
-      <ForgotPasswordContent />
-    </Suspense>
-  );
+  return <ForgotPasswordContent />;
 }
