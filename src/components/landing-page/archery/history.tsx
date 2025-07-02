87342406dfeb8d69d52650cc4e645f0e452245dd
Maelton Lima <maelton.main@gmail.com>
2025-07-02 18:06:44 -0300

    chore: modify archery page

--- src/components/landing-page/archery/history.tsx
+++ src/components/landing-page/archery/history.tsx
@@ -4,19 +4,35 @@ export const HistorySection = () => {
       <div className='container'>
         <div className='mb-12 text-center'>
           <h2 className='mb-4 text-3xl font-bold text-blue-800'>
-            História da Arquearia
+            Torne-se um Praticante de Arquearia
           </h2>
           <div className='mx-auto mb-6 h-1 w-20 bg-yellow-500'></div>
           <p className='mx-auto max-w-3xl text-gray-600'>
-            Conheça a evolução desta arte milenar desde suas origens como
-            ferramenta de caça e guerra até sua transformação em caminho de
-            desenvolvimento espiritual.
+            Conheça a prática da Arquearia na Budokan! Arte milenar como ferramenta 
+            de caça e guerra transformada em caminho de desenvolvimento espiritual.
           </p>
         </div>
 
         <div className='grid gap-12 md:grid-cols-2'>
           <div className='space-y-6'>
             <div>
+              <h3 className='mb-2 text-xl font-bold text-blue-800'>
+                Arquearia na Budokan-Ryu
+              </h3>
+              <p className='text-gray-600'>
+                <p>Se deseja conhecer sobre a prática do tiro com arco e pretende dar os primeiros 
+                passos no universo da arquearia ou já seja um arqueiro experiente em busca de aprimoramento, 
+                a Budokan Arquearia atende às suas necessidades.</p><br></br>
+
+                <p>Aqui, você encontrará informações sobre onde praticar tiro com arco em Alagoas, 
+                a agenda de torneios em andamento, além de artigos, guias e conteúdos especializados 
+                para elevar seu desempenho no esporte.</p><br></br>
+
+                <p>Também disponibilizamos indicações de lojas para compra de equipamentos e acessórios, 
+                bem como uma lista de instrutores e clubes certificados, prontos para oferecer orientação qualificada.</p><br></br>
+              </p>
+            </div>
+            {/* <div>
               <h3 className='mb-2 text-xl font-bold text-blue-800'>
                 Origens Ancestrais
               </h3>
@@ -27,8 +43,8 @@ export const HistorySection = () => {
                 (794-1185), o tiro com arco a cavalo (Yabusame) tornou-se uma
                 habilidade essencial para a classe guerreira.
               </p>
-            </div>
-            <div>
+            </div> */}
+            {/* <div>
               <h3 className='mb-2 text-xl font-bold text-blue-800'>
                 Influência Zen
               </h3>
@@ -39,8 +55,8 @@ export const HistorySection = () => {
                 influência estabeleceu as bases filosóficas do Arquearia
                 moderno.
               </p>
-            </div>
-            <div>
+            </div> */}
+            {/* <div>
               <h3 className='mb-2 text-xl font-bold text-blue-800'>
                 Era Moderna
               </h3>
@@ -51,34 +67,37 @@ export const HistorySection = () => {
                 Arquearia, padronizando estilos e técnicas. Hoje, é praticado em
                 todo o mundo como caminho de desenvolvimento pessoal.
               </p>
-            </div>
+            </div> */}
           </div>
           <div className='space-y-6'>
-            <div>
+            {/* <div>
               <h3 className='mb-2 text-xl font-bold text-blue-800'>
                 Arquearia na Budokan-Ryu
               </h3>
               <p className='text-gray-600'>
-                Na Budokan-Ryu, o Arquearia é ensinado seguindo a tradição
-                japonesa autêntica, com instrutores qualificados que receberam
-                treinamento no Japão. Nossa abordagem enfatiza tanto a técnica
-                precisa quanto os aspectos filosóficos e espirituais desta arte.
+                <p>Se deseja conhecer sobre a prática do tiro com arco e pretende dar os primeiros 
+                passos no universo da arquearia ou já seja um arqueiro experiente em busca de aprimoramento, 
+                a Budokan Arquearia atende às suas necessidades.</p><br></br>
+
+                <p>Aqui, você encontrará informações sobre onde praticar tiro com arco em Alagoas, 
+                a agenda de torneios em andamento, além de artigos, guias e conteúdos especializados 
+                para elevar seu desempenho no esporte.</p><br></br>
+
+                <p>Também disponibilizamos indicações de lojas para compra de equipamentos e acessórios, 
+                bem como uma lista de instrutores e clubes certificados, prontos para oferecer orientação qualificada.</p><br></br>
               </p>
-            </div>
+            </div> */}
             <div>
               <h3 className='mb-2 text-xl font-bold text-blue-800'>
                 Prática e Eventos
               </h3>
               <p className='text-gray-600'>
-                Realizamos sessões regulares de prática em nosso dojo
-                especialmente adaptado para Arquearia, além de participar de
-                demonstrações, competições e seminários com mestres visitantes.
-                A Budokan-Ryu também organiza eventos especiais como o Toshiya
-                (competição tradicional de tiro com arco) e cerimônias de
-                abertura do ano.
+                <p>Realizamos treinos regulares de prática de tiro com arco, além de participar de demonstrações, competições e seminários com mestres visitantes.</p><br></br>
+                
+                <p>A Budokan-Ryu Arquearia organiza torneios de tiro com arco para pontuação na Federação Field Brasil.</p>
               </p>
             </div>
-            <div>
+            {/* <div>
               <h3 className='mb-2 text-xl font-bold text-blue-800'>
                 Filosofia e Valores
               </h3>
@@ -89,7 +108,7 @@ export const HistorySection = () => {
                 em forma. Estes princípios transcendem o dojo e são aplicados na
                 vida cotidiana de nossos praticantes.
               </p>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
