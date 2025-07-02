83cb5d3ae839f0bb20b25d84f71268cadf531ffb
Maelton Lima <maelton.main@gmail.com>
2025-07-02 17:40:19 -0300

    chore: add walter moura testimony

--- src/data/instructors.ts
+++ src/data/instructors.ts
@@ -9,7 +9,7 @@ export const instructors = [
     description:
       'Fundador da Budokan-Ryu, treina Karate há mais de 40 anos. Formado diretamente pelo Sensei Okuda e 5º Dan, é reconhecido nacionalmente por sua técnica e dedicação ao ensino.',
     imageSrc: senseiKarateAsset.src,
-    alt: 'Sensei Tanaka',
+    alt: 'Sensei Chicão',
   },
   {
     name: 'Sensei Hsie',
@@ -17,7 +17,7 @@ export const instructors = [
     description:
       'Com mais de 25 anos de experiência, foi responsável pela divisão de Kendo da Budokan-Ryu. Participou de diversos campeonatos mundiais e é referência na formação de novos kendokas.',
     imageSrc: senseiKendoAsset.src,
-    alt: 'Sensei Yamamoto',
+    alt: 'Sensei Hsie',
   },
   {
     name: 'Sensei Yasuhiro',
@@ -25,6 +25,6 @@ export const instructors = [
     description:
       'Especialista em Arquearia, treinou por mais de 15 anos no Japão antes de retornar ao Brasil para difundir esta arte milenar. É reconhecido por sua precisão técnica e profundo conhecimento filosófico.',
     imageSrc: senseiArcheryAsset.src,
-    alt: 'Sensei Sato',
+    alt: 'Sensei Yasuhiro',
   },
 ];
--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -1,5 +1,6 @@
 import AvatarAldemarAraujo from '@/assets/images/avatar-aldemar-araujo.png';
 import AvatarLilianCarmen from '@/assets/images/avatar-lilian-carmen.jpg';
+import AvatarWalterMoura from '@/assets/images/avatar-walter-moura.jpeg';
 
 export interface Testimonial {
   id: number;
@@ -29,4 +30,15 @@ export const testimonialsData: Testimonial[] = [
     quote:
       '"Comecei a praticar tiro com arco em 2019 e, ao longo dos treinos, fui desenvolvendo a forma de execução correta para um tiro preciso. Em 2021 inicio a prática de tiro com arco na Budokan Arquearia, onde aprimorei, com a prática constante, a percepção do corpo, controle da respiração, forma de segurar o arco e a tensão aplicada à corda. Princípios fundamentais que ajudam na formação intrapessoal e interpessoal."',
   },
+    {
+    id: 3,
+    name: 'Walter Moura',
+    discipline: 'Kendo',
+    title: 'Praticante de Kendo',
+    image: AvatarWalterMoura.src,
+    quote:
+      '"Há 10 anos, Sempai Alencar me levou para uma aula de Kendo. Eu estava passando por vários problemas na minha vida pessoal e sempre muito resistente a participar de uma única aula. Me lembro como se fosse hoje: quando cheguei ao dojo, me deparei com um senhor dos seus quase 70 anos, com olhos puxados e de um semblante sério — Sensei Shasan. A partir daquele dia, entendi que a disciplina e o amor pelo que fazemos e acreditamos pode mudar nossas vidas. Assim foi com a minha. Oss!!!"',
+  },
+
+  
 ];
