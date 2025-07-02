8c241631ce957110901258f8f1157c553c630b9f
Maelton Lima <maelton.main@gmail.com>
2025-07-02 17:29:28 -0300

    chore: add kendo and archery senseis on landing page

--- src/data/instructors.ts
+++ src/data/instructors.ts
@@ -1,28 +1,30 @@
 import senseiKarateAsset from '@/assets/images/sensei-karate.jpg';
+import senseiArcheryAsset from '@/assets/images/sensei-archery.jpeg';
+import senseiKendoAsset from '@/assets/images/sensei-kendo.jpeg';
 
 export const instructors = [
   {
-    name: 'Sensei Chicão - Francisco Araújo',
-    title: '5° Dan',
+    name: 'Sensei Chicão',
+    title: 'Francisco Araújo',
     description:
-      'Fundador da Budokan-Ryu, treina Karate há mais de 40 anos. Formado diretamente pelo Sensei Okuda, é reconhecido nacionalmente por sua técnica e dedicação ao ensino.',
+      'Fundador da Budokan-Ryu, treina Karate há mais de 40 anos. Formado diretamente pelo Sensei Okuda e 5º Dan, é reconhecido nacionalmente por sua técnica e dedicação ao ensino.',
     imageSrc: senseiKarateAsset.src,
     alt: 'Sensei Tanaka',
   },
   {
-    name: 'Sensei Yamamoto',
-    title: 'Kendo - 5º Dan',
+    name: 'Sensei Hsie',
+    title: 'Hsie Sashan',
     description:
-      'Com mais de 25 anos de experiência, é responsável pela divisão de Kendo da Budokan-Ryu. Participou de diversos campeonatos mundiais e é referência na formação de novos kendokas.',
-    imageSrc: null,
+      'Com mais de 25 anos de experiência, foi responsável pela divisão de Kendo da Budokan-Ryu. Participou de diversos campeonatos mundiais e é referência na formação de novos kendokas.',
+    imageSrc: senseiKendoAsset.src,
     alt: 'Sensei Yamamoto',
   },
   {
-    name: 'Sensei Sato',
-    title: 'Arquearia - 6º Dan',
+    name: 'Sensei Yasuhiro',
+    title: 'Yasuhiro Higashikawauchi',
     description:
       'Especialista em Arquearia, treinou por mais de 15 anos no Japão antes de retornar ao Brasil para difundir esta arte milenar. É reconhecido por sua precisão técnica e profundo conhecimento filosófico.',
-    imageSrc: null,
+    imageSrc: senseiArcheryAsset.src,
     alt: 'Sensei Sato',
   },
 ];
